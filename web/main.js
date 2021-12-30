174c7e4b4930eeaebf8797164c09f5d4e7b81ff4
Thomas Ballinger <thomasballinger@gmail.com>
2021-12-29 22:57:19 -0800

    fewer levels of indirection and using local CSS assets

--- web/main.js
+++ web/main.js
@@ -2,15 +2,12 @@ import { app } from "@tauri-apps/api";
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-// This is the normal way to include react semantic ui css, but
-// loading from html avoids needing esbuild binary loaders.
-// import "semantic-ui-css/semantic.css";
-
 import { App } from "./src/app";
+
+const TwinleafApp = React.createElement(App, {}, null);
+ReactDOM.render(TwinleafApp, document.querySelector('#root'));
 
 // In a browser, globalThis points to the window object.
 globalThis.App = App;
 globalThis.React = React;
 globalThis.ReactDOM = ReactDOM;
-
